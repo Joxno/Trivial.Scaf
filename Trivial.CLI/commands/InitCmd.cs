dd29d23bd1db63b3505aca38429443573e9f16db
Sebastian Georghiou <Joxno@users.noreply.github.com>
2025-02-21 17:21:27 +0000

    Implemented only-config mode for initialising a new template.

--- Trivial.CLI/commands/InitCmd.cs
+++ Trivial.CLI/commands/InitCmd.cs
@@ -32,7 +32,7 @@ public static class InitCmd
         });
 
         var t_InitTemplateCmd = t_InitCmd.NewSub("template", "Initialises a template", 
-                (Name, Key, Description, Output) => {
+                (Name, Key, Description, Output, OnlyConfig) => {
                     var t_ResolvedPath = ScafPaths.ResolvePath(Output ?? $"./{Name}");
                     if(!Directory.Exists(t_ResolvedPath))
                         Directory.CreateDirectory(t_ResolvedPath);
@@ -42,13 +42,16 @@ public static class InitCmd
                         [ new("", "", [], [], new("Run-Default.ps1", ["Includes.ps1"], [new("ScafCfg", "template.scaf.json")])) ]);
                     var t_Serialized = JsonSerializer.Serialize(t_NewTemplate, new JsonSerializerOptions { WriteIndented = true });
                     File.WriteAllText(Path.Combine(t_ResolvedPath, $"template.scaf.json"), t_Serialized);
+                    
+                    if(OnlyConfig) return;
                     File.WriteAllText(Path.Combine(t_ResolvedPath, "Run-Default.ps1"), "Hello-World");
                     File.WriteAllText(Path.Combine(t_ResolvedPath, "Includes.ps1"), "function Hello-World() {\n\tWrite-Host \"Hello, World! from $($ScafCfg.Name) template.\"\n}");
                 },
                 new Argument<string>("name", "The name of the template"),
                 new Argument<string>("key", "The key for the template"),
                 new Option<string>(["-d", "--description"], () => "", "The description of the template"),
-                new Option<string?>(["-o", "--output"], () => null, "The output directory for the template")
+                new Option<string?>(["-o", "--output"], () => null, "The output directory for the template"),
+                new Option<bool>(["--only-config"], () => false, "Only creates the template config file")
             );
 
             var t_InitRepoCmd = t_InitCmd.NewSub("repo", "Initialises a new remote repo", (Path, AddRemote, Name) => {
